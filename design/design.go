5541da551c58db1c8c00c4d3bb71fd91a875b55e
Itamar <itamart@wix.com>
2019-02-18 17:00:55 +0200

    init

---
+++ design/design.go
@@ -0,0 +1,39 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = API("yawarad", func() {
+	Title("YAWARAD API for the lazy DBA")
+	Description("Set of utiles for creating and managing databases")
+	Host("localhost:8081")
+	BasePath("/api")
+})
+
+// Media Types
+var ClusterForBranching = MediaType("application/vnd.clusterForBranching+json", func() {
+	Description("A Database Cluster with all the relevant data for creating new cluster branch from a snapshot")
+	Attributes(func() {
+		Attribute("id", UUID, "Uniq Id from Shapherd")
+		Attribute("name", String, "Cluster name")
+		Attribute("servers", ArrayOf(String, func() {
+			Format("hostname")
+			Example("db-mysql-my-cluster0a.42.wixprod.net")
+			Example("db-mysql-my-cluster1b.42.wixprod.net")
+			Example("db-mysql-my-cluster0a.96.wixprod.net")
+			Example("db-mysql-my-cluster1b.96.wixprod.net")
+			Example("db-mysql-my-cluster0a.42.wixprod.net")
+		}), "List of server in cluster")
+		Attribute("master_hostname", String, "The current RW master of the cluster", func() {
+			Format("hostname")
+			Example("db-mysql-my-cluster0a.42.wixprod.net")
+		})
+		Attribute("backup_hostname", String, "The current backup server of the cluster", func() {
+			Format("hostname")
+			Example("db-mysql-my-cluster0a.42.wixprod.net")
+		})
+
+	})
+})
